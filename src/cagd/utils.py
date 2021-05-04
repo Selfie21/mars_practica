e4426ea79af69cd9b4e9d72689cce08f78d91561
Felix Dittmann <felix-dittmann@gmx.de>
2021-05-04 16:15:49 +0200

    implememting solve_tridiagonal_equation method in utils.py. might need some testing when other methods are implemented

--- src/cagd/utils.py
+++ src/cagd/utils.py
@@ -7,9 +7,28 @@ from cagd.vec import vec2
 #all four parameters are vectors of size n
 #the first element of diag1 and the last element of diag3 are ignored
 #therefore diag1[i], diag2[i] and diag3[i] are located on the same row of A
+#a = diag1 b = diag2 c=diag3
 def solve_tridiagonal_equation(diag1, diag2, diag3, res):
     assert(len(diag1) == len(diag2) == len(diag3) == len(res))
-    solution = None
+    alpha[len(diag2)]
+    teta[len(diag2)]
+    beta[len(diag2)]
+    beta[0] = diag2[0]
+    for i in range(1, len(diag2)):
+        alpha[i] = diag1[i]
+        teta[i] = diag3[i]/beta[i-1]
+        beta[i] = diag2[i] - alpha[i]*teta[i]
+
+    y[len(res)]
+    y[0] = res[0]/beta[0]
+    for i in range(1, len(res)):
+        y[i] = res[i]-alpha[i]*y[i-1]/beta[i]
+
+    x[len(res)] = y[len(res)]
+    for i in range(len(res)-1, 0):
+        x[i] = res[i]-teta[i+1]*x[i+1]
+
+    solution = x
     return solution
      
 
